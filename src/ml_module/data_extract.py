9dd5cf7e2a70cc543ac31e5638d1c2c293817b04
ruibing <rbruibingnet@gmail.com>
2023-05-02 04:20:11 +0800

    update file path

--- src/ml_module/data_extract.py
+++ src/ml_module/data_extract.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def extract_data():
     
-    conn = sqlite3.connect('../data/fishing.db')
+    conn = sqlite3.connect('data/fishing.db')
     query = "SELECT * FROM fishing"
     df = pd.read_sql_query(query, conn)
     conn.close()
