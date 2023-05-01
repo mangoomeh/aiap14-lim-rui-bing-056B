299ea5d5d100d01114ccdd7226194e1a5bb9c59d
ruibing <rbruibingnet@gmail.com>
2023-05-01 17:49:35 +0800

    added feature engineering method

--- src/ml_module/data_prep.py
+++ src/ml_module/data_prep.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.impute import SimpleImputer
 
 def clean_data(df):
@@ -15,9 +16,6 @@ def clean_data(df):
     # fix casing problems in pressure data
     clean_df.loc[:, "Pressure9am"] = clean_df["Pressure9am"].str.lower()
     clean_df.loc[:, "Pressure3pm"] = clean_df["Pressure3pm"].str.lower()
-
-    # impute missing values
-    clean_df = impute_missing_values(clean_df)
 
     return clean_df
 
@@ -37,3 +35,17 @@ def impute_missing_values(df):
     imputed_df.loc[:, categorical_cols] = mode_imputer.fit_transform(imputed_df[categorical_cols])
 
     return imputed_df
+
+def feature_engineer(df):
+
+    # convert datatype to datetime
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    # create day and month column in the dataframe
+    df["Month"] = df["Date"].dt.month
+    df["Day"] = df["Date"].dt.day
+
+    # drop date column
+    df = df.drop(columns=["Date"])
+
+    return df
