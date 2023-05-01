63169598d32591e358c0ec27ace2deb66c679912
ruibing <rbruibingnet@gmail.com>
2023-05-01 20:58:53 +0800

    improve readability

--- src/ml_module/data_extract.py
+++ src/ml_module/data_extract.py
@@ -2,6 +2,9 @@ import pandas as pd
 import sqlite3
 
 def extract_data():
+    
     conn = sqlite3.connect('fishing.db')
     query = "SELECT * FROM fishing"
-    return pd.read_sql_query(query, conn)
+    df = pd.read_sql_query(query, conn)
+    
+    return df
