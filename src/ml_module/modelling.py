78ed5d2660bffaf6d1c490253454ec3748500f06
ruibing <rbruibingnet@gmail.com>
2023-05-01 20:58:41 +0800

    created modelling method

---
+++ src/ml_module/modelling.py
@@ -0,0 +1,26 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
+
+def model(df):
+
+    # get y-values (series)
+    y = df["RainTomorrow"]
+
+    # get features (dataframe)
+    X = df.loc[:, df.columns != "RainTomorrow"]
+    
+    # split data
+    X_train, y_train, X_test, y_test = train_test_split(X, y, random_state=20, train_size=.8)
+
+    # create models
+    models = []
+    lr = LogisticRegression(max_iter=1000)
+    models.append(lr)
+    
+    # fit, predict and show metrics
+    for model in models:
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        print(accuracy_score(y_test, y_pred))
+        print(f1_score(y_test, y_pred))
