55095d7d97dbbf99a59dbf8dbd5b8b6ad4bdfd9b
ruibing <rbruibingnet@gmail.com>
2023-05-02 15:51:04 +0800

    added mlpclassifier and randomforest and updated the print statements for metrics

--- src/ml_module/modelling.py
+++ src/ml_module/modelling.py
@@ -1,7 +1,10 @@
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
 
 def model(df):
 
@@ -10,6 +13,9 @@ def model(df):
 
     # get features (dataframe)
     X = df.loc[:, df.columns != "RainTomorrow"]
+
+    # feature scaling
+    X = StandardScaler().fit_transform(X)
     
     # split data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=20, train_size=.8)
@@ -17,11 +23,17 @@ def model(df):
     # create models
     models = []
     models.append(LogisticRegression(max_iter=5000))
-    models.append(KNeighborsClassifier(n_neighbors=15))
+    models.append(KNeighborsClassifier(n_neighbors=12))
+    models.append(MLPClassifier(hidden_layer_sizes=(150,100,50), 
+                                max_iter=300,activation = 'relu',
+                                solver='adam',
+                                random_state=1))
+    models.append(RandomForestClassifier())
     
     # fit, predict and show metrics
     for model in models:
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        print(accuracy_score(y_test, y_pred))
-        print(f1_score(y_test, y_pred))
+        print(type(model).__name__)
+        print("Accuracy Score:", accuracy_score(y_test, y_pred))
+        print("F1 Score:", f1_score(y_test, y_pred))
